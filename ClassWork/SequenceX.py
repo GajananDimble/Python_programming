7685b2fae6ab3330e57501f839a59bc77adffc4a
Gajanan Sunil Dimble <gajanandimble2355@gmail.com>
2026-06-22 19:27:54 +0530

    Add files via upload

---
+++ ClassWork/SequenceX.py
@@ -0,0 +1,16 @@
+Data1=[10,20,30,30]                   #list
+Data2=(10,20,30,40)                   #tuple
+Data3={10,20,30,40}                   #set
+Data4={"A":10,"B":20,"C":30,"D":40}   #dict
+Data5="Maharashtra"                   #str
+
+print(Data1)
+print(type(Data1))
+print(Data2)
+print(type(Data2))
+print(Data3)
+print(type(Data3))
+print(Data4)
+print(type(Data4))
+print(Data5)
+print(type(Data5))
