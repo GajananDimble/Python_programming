7a0e22331c6b7cf1962436c6bd95fc6126f50108
Gajanan Sunil Dimble <gajanandimble2355@gmail.com>
2026-06-26 22:09:10 +0530

    Add files via upload

---
+++ ClassWork/Function1.py
@@ -0,0 +1,13 @@
+def main():
+    print("Enter First Number:")               #12-13-1-2-3-8-10-12-13
+    value1=int(input())
+
+    print("Enter Second Number:")
+    value2=int(input())
+
+    Ret=value1+value2
+
+    print("Addition is:",Ret)
+
+if __name__ =="__main__":
+    main()
