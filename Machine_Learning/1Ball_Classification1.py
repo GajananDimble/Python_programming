6a3a969b92d9683489866bc87a6edcf972c425d1
Gajanan Sunil Dimble <gajanandimble2355@gmail.com>
2026-08-08 12:36:26 +0530

    Add files via upload

---
+++ Machine_Learning/1Ball_Classification1.py
@@ -0,0 +1,14 @@
+def main():
+    print("Ball Classification Case Study")
+
+    Features=[[35,"Rough"],[47,"Rough"],[90,"Smooth"],[48,"Rough"],[90,"Smooth"],[35,"Rough"],[92,"Smooth"],[35,"Rough"],[35,"Rough"],[35,"Rough"],[96,"Smooth"],[43,"Rough"],[110,"Smmoth"],[35,"Rough"],[96,"Smmoth"]]
+    
+    Labels=["Tennis","Tennis","Cricket","Tennis","Cricket","Tennis","Cricket","Tennis","Tennis","Tennis","Cricket","Tennis","Cricket","Tennis","Cricket"]
+
+
+
+    print("Features Are:",Features)
+    print("Lebels are:",Labels)
+
+if __name__=="__main__":
+    main()
