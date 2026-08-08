4b609e54ff7d80a781cedc0af8edd133160e9d7d
Gajanan Sunil Dimble <gajanandimble2355@gmail.com>
2026-08-08 12:37:18 +0530

    Add files via upload

---
+++ Machine_Learning/2Ball_Classification2.py
@@ -0,0 +1,13 @@
+def main():
+    print("Ball Classification Case Study")
+
+    Features=[[35,1],[47,1],[90,0],[48,1],[90,0],[35,1],[92,0],[35,1],[35,1],[35,1],[96,0],[43,1],[110,0],[35,1],[96,0]]
+    
+    Labels=[1,1,2,1,2,1,2,1,1,1,2,1,2,1,2]
+
+   # Encoding
+    print("Features Are:",Features)
+    print("Lebels are:",Labels)
+
+if __name__=="__main__":
+    main()
