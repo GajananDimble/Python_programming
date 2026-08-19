416a9a34b39b3638753e06283751095847c99c58
Gajanan Sunil Dimble <gajanandimble2355@gmail.com>
2026-08-19 19:54:48 +0530

    Add files via upload

---
+++ Machine_Learning/39_UserDefined_KNN1.py
@@ -0,0 +1,24 @@
+
+def MarvellousKNNClassifier():
+    border="_"*30
+
+    Data=[
+        {"Point":"A","X":1,"Y":2,"Label":"Red"},
+        {"Point":"B","X":2,"Y":3,"Label":"Red"},
+        {"Point":"C","X":3,"Y":1,"Label":"Blue"},
+        {"Point":"D","X":5,"Y":6,"Label":"Blue"}
+    ]
+    print(border)
+    print("Marvellous KNN classifier")
+    print(border)
+
+    print(Data)
+    print(border)
+    
+def main():
+    MarvellousKNNClassifier()
+
+
+
+if __name__=="__main__":
+    main()
